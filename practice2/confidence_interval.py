17c4e5d65c5796d6bcae253c7e9736b1cc0c478e
kisun.han <kisun.han@gmail.com>
2025-08-13 23:29:01 +0900

    parameterized test

--- practice2/confidence_interval.py
+++ practice2/confidence_interval.py
@@ -1,18 +1,46 @@
 from typing import List
 
+def calculate_sum_and_squared_sum(samples: List[int]):
+    return sum(samples), sum([i**2 for i in samples])
+
+def _mean(total, n):
+    return total / n
+
+def _std(sum, squared_sum, n):
+    # var = E(X^2) - E(X)^2
+    return (_mean(squared_sum, n) - _mean(sum, n) ** 2) ** 0.5
+
+def _get_ci_point(ci):
+    if ci == 95:
+        return 1.96
+    if ci == 99:
+        return 2.57
+    raise ValueError(f"Invalid confidence interval {ci}")
+
+def ci_min(mean, std, n, ci):
+    return mean - (_get_ci_point(ci) * std / (n**0.5))
+
+def ci_max(mean, std, n, ci):
+    return mean + (_get_ci_point(ci) * std / (n**0.5))
+
 def ci95(samples: List[int]):
     n = len(samples)
-    my_sum = sum(samples)
-    squared_sum = sum([i**2 for i in samples])
-    mean = my_sum / n
-    mean_square = mean ** 2
-    mean_squared_sum = squared_sum / n
-    # var = E(X^2) - E(X)^2
-    var = mean_squared_sum - mean_square
-    std = var ** 0.5
+    sum, squared_sum = calculate_sum_and_squared_sum(samples)
+    mean = _mean(sum, n)
+    std = _std(sum, squared_sum, n)
 
-    ci_min = mean - (1.96 * std / (n**0.5))
-    ci_max = mean + (1.96 * std / (n**0.5))
+    return ci_min(mean, std, n, ci=95), ci_max(mean, std, n, ci=95)
 
-    return ci_min, ci_max
+def ci95_round(samples: List[int], round_num:int):
+    ci_min, ci_max = ci95(samples)
+    return round(ci_min, round_num), round(ci_max, round_num)
+
+
+def ci99(samples: List[int]):
+    n = len(samples)
+    sum, squared_sum = calculate_sum_and_squared_sum(samples)
+    mean = _mean(sum, n)
+    std = _std(sum, squared_sum, n)
+
+    return ci_min(mean, std, n, ci=99), ci_max(mean, std, n, ci=99)
     # return 1.7603871572139953, 4.239612842786005
--- practice2/test_confidence_interval.py
+++ practice2/test_confidence_interval.py
@@ -14,6 +14,7 @@ n = 5
 mean +- z-score * std / root(n)
 ci_max, ci_min
 """
+import pytest
 import confidence_interval as ci
 
 def test_ci95():
@@ -33,3 +34,34 @@ def test_ci95():
     assert expected_ci_min == actual_ci_min
     assert expected_ci_max == actual_ci_max
 
+@pytest.mark.parametrize("samples, expected_min, expected_max", [
+    ([1,2,3,4,5], 1.76, 4.24),
+    ([6,7,8,9,10], 6.76, 9.24),
+    ([11,12,13,14,15], 11.76, 14.24),
+    ([1,3,5,7,9,11,13], 4.037, 9.963)
+])
+def test_ci95_round(samples, expected_min, expected_max):
+    # given
+
+    # when
+    actual_min, actual_max = ci.ci95_round(samples,3)
+    # then
+    assert actual_min == expected_min
+    assert actual_max == expected_max
+
+def test_ci99():
+    # given (preparing)
+    samples = [1,2,3,4,5]
+    mean = 3
+    std = 2 ** 0.5
+    n = len(samples)
+
+    expected_ci_min = mean - (2.57 * (std / (n**0.5)))
+    expected_ci_max = mean + (2.57 * (std / (n**0.5)))
+
+    # when
+    actual_ci_min, actual_ci_max = ci.ci99(samples)
+
+    # then
+    assert expected_ci_min == actual_ci_min
+    assert expected_ci_max == actual_ci_max
